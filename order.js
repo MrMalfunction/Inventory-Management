faf06142fa59dd03ba451cd096230e3a0f88a12b
Amol Bohora <007bohora@gmail.com>
2020-12-11 20:40:52 +0530

    Added Come changes

---
+++ order.js
@@ -0,0 +1,47 @@
+function check(){
+	remove = 0;
+	flag = 0;
+	name = ($('#name').val());
+	if(name === ""){
+		$('#name').css({"border-color" :"red","box-shadow" :"5px 10px red"});
+		return null;
+	}
+	else{
+		$('#name').css({"border-color" :"","box-shadow" :""})
+	}
+	if(count === 2){
+		 if($('#item'+(count-1)).val() === ""){
+      $('#item'+(count-1)).css({"border-color" :"red","box-shadow" :"5px 10px red"});
+      return null;
+    }
+    else{
+      $('#item'+(count-1)).css({"border-color" :"","box-shadow" :""});
+    }
+
+    if($('#quantity'+(count-1)).val() === ""){
+      $('#quantity'+(count-1)).css({"border-color" :"red","box-shadow" :"5px 10px red"});
+      return null;
+    }
+    else{
+      $('#quantity'+(count-1)).css({"border-color" :"","box-shadow" :""});
+    }
+
+    if($('#price'+(count-1)).val() === ""){
+      $('#price'+(count-1)).css({"border-color" :"red","box-shadow" :"5px 10px red"});
+      return null;
+    }
+    else{
+      $('#price'+(count-1)).css({"border-color" :"","box-shadow" :""});
+    }
+	}
+	for (var i = 1; i < count; i++) {
+		item = ($('#item'+i).val());
+		quantity = ($('#quantity'+i).val());
+		price = ($('#price'+i).val());
+		if(item === "" && quantity === "" && price ===""){
+			$(".additional"+i).remove();
+			++remove;
+		}
+	}
+	count = count - remove;
+}
